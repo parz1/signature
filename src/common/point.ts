c26a3c434b46f96e96352d11a4af1e9362b1f354
parz1 <parz1zhou@gmail.com>
2021-07-27 14:00:43 +0800

    js ver init

---
+++ src/common/point.ts
@@ -0,0 +1,19 @@
+export default class Point {
+  x: number
+  y: number
+  time: Date
+  constructor(x: number, y: number, time: Date) {
+    this.x = x;
+    this.y = y;
+    this.time = time || Date.now();
+  }
+  distanceTo(start: Point) {
+    return Math.sqrt(Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2));
+  }
+  equals(other: Point) {
+    return this.x === other.x && this.y === other.y && this.time === other.time;
+  }
+  velocityFrom(start: Point): number {
+    return (this.time !== start.time) ? this.distanceTo(start) / (this.time.getTime() - start.time.getTime()) : 0;
+  }
+}
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,50 +1,164 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+  <view class="content">
+    <div>Handwriting</div>
+    <div class="canvas-box">
+      <canvas
+        type="2d"
+        class="handwriting"
+        disable-scroll="true"
+        @touchstart="uploadScaleStart"
+        @touchmove="uploadScaleMove"
+        @touchend="uploadScaleEnd"
+        canvas-id="handwriting"
+        id="handwriting"
+      ></canvas>
+    </div>
+    <div>
+      <button @tap="retDraw" class="del-btn">清除</button>
+      <button @tap="subCanvas" class="sub-btn">保存</button>
+      <button @tap="onConfirm" class="sub-btn">保存到本地</button>
+    </div>
+    <div>
+      <img :src="filePath" alt />
+    </div>
+  </view>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-	export default Vue.extend({
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+import Vue from 'vue';
+import SignaturePad from '../../common/signaturePad.js'
 
-		},
-		methods: {
+let penColor = 'black';
+let lineWidth = 0.6;
 
-		}
-	});
+export default Vue.extend({
+  data() {
+    return {
+      signaturePad: SignaturePad,
+      penColor: 'black',
+      lineWidth: 0.6,
+      isEmpty: true,
+      filePath: '',
+      pix: 7,
+      width: 0,
+      height: 0,
+    }
+  },
+  onLoad() {
+    let self = this
+    const query = wx.createSelectorQuery()
+    query.select('#handwriting')
+      .fields({ node: true, size: true })
+      .exec((res) => {
+        const canvas = res[0].node
+        const ctx = canvas.getContext('2d')
+        console.log(ctx)
+        const data = {
+          devicePixelRatio: this.pix,
+          minWidth: 0.6,
+          maxWidth: 5,
+          backgroundColor: 'rgba(255, 255, 255, 0)',
+          penColor: "rgb(66, 133, 244)"
+        };
+
+        const dpr = wx.getSystemInfoSync().pixelRatio
+        canvas.width = res[0].width * dpr
+        canvas.height = res[0].height * dpr
+        ctx.scale(dpr, dpr)
+
+        self.signaturePad = new SignaturePad(canvas, ctx, data)
+      })
+
+    // let self = this
+    // wx.getSystemInfo({
+    //   success: function (res) {
+    //     self.pix = res.pixelRatio
+    //     self.width = res.windowWidth * self.pix
+    //     self.height = res.windowHeight * self.pix
+    //   }
+    // })
+    // const query = wx.createSelectorQuery()
+    // const canvas = query.select("#handwriting")
+    // let ctx = canvas.context(res => {
+    //   console.log(res.context)
+    //   // res.getContext('2d')
+    //   // console.log(res)
+    // })
+    // this.signaturePad = new SignaturePad(ctx)
+    // canvas.context(res => {
+    //   console.log(res)
+    //   ctx = res.context
+    //   const data = {
+    //     devicePixelRatio: this.pix,
+    //     minWidth: 0.6,
+    //     maxWidth: 5,
+    //     backgroundColor: 'rgba(255, 255, 255, 0)',
+    //     penColor: "rgb(66, 133, 244)"
+    //   };
+    //   this.signaturePad = new SignaturePad(ctx, data)
+    //   console.log(this.signaturePad)
+    // }).exec()
+
+  },
+  methods: {
+    uploadScaleStart(e) {
+      const item = {
+        penColor: penColor,
+        lineWidth: lineWidth
+      };
+      this.signaturePad.handleTouchStart(e, item);
+    },
+    uploadScaleMove(e) {
+      this.signaturePad.handleTouchMove(e);
+    },
+    uploadScaleEnd(e) {
+      this.signaturePad.handleTouchEnd(e);
+      // const isEmpty = signaturePad.isEmpty();
+      // this.isEmpty = isEmpty
+    },
+    retDraw() {
+      this.signaturePad.clear();
+      const isEmpty = this.signaturePad.isEmpty();
+      this.isEmpty = isEmpty
+    },
+    subCanvas: function () {
+      if (this.isEmpty) {
+        return false
+      }
+    },
+    onConfirm: function () {
+      if (this.isEmpty) {
+        return false
+      }
+      const self = this;
+      wx.canvasToTempFilePath({
+        canvasId: 'handwriting',
+        success: function (res) {
+          console.log(res.tempFilePath)
+          self.filePath = res.tempFilePath
+        },
+        fail: function (res) {
+          console.log(res)
+        },
+        complete: function (res) {
+          console.log(res)
+        }
+      })
+    },
+  }
+});
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.canvas-box {
+  margin: 30rpx;
+  height: 500rpx;
+  border: 1px solid #666;
+  border-radius: 50rpx;
+}
+#handwriting {
+  /* width: 80vw;
+  height: 50vh; */
+  height: 100%;
+}
 </style>
